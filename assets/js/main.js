9a2db49cf1b076ac6ba2c3d7ac3f7463e3aee093
MD. MONWAR SIDDIQ <monwarsiddiq@gmail.com>
2021-12-10 23:26:50 +0600

    Responsive for Mobile, Tablet and Small Desktop Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,6 @@
 $(document).ready(function() {
-    $('.owl-carousel:not(".clients-logos, .project-carousel")').owlCarousel({
-        loop:true,
-        margin: 30,
-        nav:false,
-        autoplay:true,
-        autoplayTimeout:2000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:3
-            }
-        }
-    }),
 
-    $('.project-carousel').owlCarousel({
+    $('.owl-carousel').owlCarousel({
         loop:false,
         margin: 30,
         nav:false,
@@ -33,26 +14,6 @@ $(document).ready(function() {
             },
             1000:{
                 items:3
-            }
-        }
-    }),
-    
-    $('.clients-logos').owlCarousel({
-        loop:true,
-        margin: 30,
-        nav:false,
-        autoplay:true,
-        autoplayTimeout:1500,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:4
-            },
-            1000:{
-                items:5
             }
         }
     })
